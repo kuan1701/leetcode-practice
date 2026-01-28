0801bdc892f85a19857f2885ac3996bea73a5798
k.a.chin <quocanh.trinh@intexsoft.by>
2026-01-28 21:21:38 +0300

    added solution of "Common elements of arrays" task

--- src/algocode/Examples.java
+++ src/algocode/Examples.java
@@ -1,50 +1,79 @@
 package algocode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Examples {
 
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 4, 5};
-        int[] nums2 = {1, 2, 3, 4, 5, 3};
+  public static void main(String[] args) {
+    int[] nums = {1, 2, 3, 4, 4, 5};
+    int[] nums2 = {1, 2, 3, 4, 5, 3};
 
-        System.out.println(defineDuplicateIndex(nums));
-        System.out.println(defineDuplicateIndex2(nums2));
+    List<Integer> integerList1 = List.of(-4, 1, 2, 3, 3, 4, 4, 5, 6, 6);
+    List<Integer> integerList2 = List.of(1, 1, 3, 3, 4, 4, 5, 6, 6, 7);
 
+    System.out.println(defineDuplicateIndex(nums));
+    System.out.println(defineDuplicateIndex2(nums2));
+    System.out.println(Arrays.toString(new List[]{commonNumsList(integerList1, integerList2)}));
+
+  }
+
+  // Дан массив целых чисел nums.
+  // Верни true, если какое-то значение встречается хотя бы два раза.
+  // Если все элементы уникальны — верни false.
+  public static boolean defineDuplicateIndex(int[] nums) {
+    // if array is sorted
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] == nums[i - 1]) {
+        System.out.println(
+            "Array " + Arrays.toString(nums) + " has duplicate at index " + (i - 1) + " and " + i);
+        return true;
+      }
     }
 
-    // Дан массив целых чисел nums.
-    // Верни true, если какое-то значение встречается хотя бы два раза.
-    // Если все элементы уникальны — верни false.
-    public static boolean defineDuplicateIndex(int[] nums) {
-        // if array is sorted
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                System.out.println(
-                        "Array " + Arrays.toString(nums) + " has duplicate at index " + (i - 1) + " and " + i);
-                return true;
-            }
-        }
+    System.out.println("Array " + Arrays.toString(nums) + " doesn't have duplicate indexes");
+    return false;
+  }
 
-        System.out.println("Array " + Arrays.toString(nums) + " doesn't have duplicate indexes");
-        return false;
+  public static boolean defineDuplicateIndex2(int[] nums) {
+    // if array is not sorted
+    Set<Integer> set = new HashSet<>();
+
+    for (int i = 1; i < nums.length; i++) {
+      if (set.contains(nums[i])) {
+        System.out.println("Array " + Arrays.toString(nums) + " has duplicate indexes");
+        return true;
+      }
+      set.add(nums[i]);
     }
 
-    public static boolean defineDuplicateIndex2(int[] nums) {
-        // if array is not sorted
-        Set<Integer> set = new HashSet<>();
+    System.out.println("Array " + Arrays.toString(nums) + " doesn't have duplicate indexes");
+    return false;
+  }
 
-        for (int i = 1; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                System.out.println("Array " + Arrays.toString(nums) + " has duplicate indexes");
-                return true;
-            }
-            set.add(nums[i]);
-        }
+  // Даны два монотонно возрастающих массива nums1 и nums2.
+  // Нужно вернуть массив nums3, который тоже будет монотонно возрастающим,
+  // и при этом содержать все общие элементы nums1 и nums2.
+  public static List<Integer> commonNumsList(List<Integer> nums1, List<Integer> nums2) {
+    List<Integer> list = new ArrayList<>();
+    int p1 = 0;
+    int p2 = 0;
 
-        System.out.println("Array " + Arrays.toString(nums) + " doesn't have duplicate indexes");
-        return false;
-     }
+    while (p1 < nums1.size() && p2 < nums2.size()) {
+      if (nums1.get(p1) < nums2.get(p2)) {
+        p1++;
+      } else if (nums1.get(p1) > nums2.get(p2)) {
+        p2++;
+      } else {
+        list.add(nums1.get(p1));
+        p1++;
+        p2++;
+      }
+    }
+
+    return list;
+  }
 }
---
+++ src/algocodeio/eachaccordingtothepoint/DefineCommonNumbers.java
@@ -0,0 +1,58 @@
+package algocodeio.eachaccordingtothepoint;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DefineCommonNumbers {
+
+  public static void main(String[] args) {
+    System.out.println("Result №1:");
+    System.out.println(defineCommonNumbers(
+        Arrays.asList(1, 1, 3, 4, 5, 5, 7, 8, 8),
+        Arrays.asList(-1, 1, 3, 3, 5, 5, 7, 7, 8)
+    ));
+
+    System.out.println("Result №2:");
+    System.out.println(defineCommonNumbers(
+        Arrays.asList(1, 1, 3, 4, 5, 5, 7, 8, 8),
+        Arrays.asList(-1, -1, 2, 2, 6, 6, 9, 9, 9)
+    ));
+  }
+
+  /**
+   * Даны два отсортированных по возрастанию массива nums1 и nums2. Необходимо вернуть новый массив
+   * nums3, который содержит все общие элементы из nums1 и nums2.
+   * <p>
+   * Результат должен быть также отсортирован по возрастанию. Если элементы встречаются в массивах
+   * несколько раз, то их нужно продублировать в ответе.
+   * <p>
+   * Пример 1: Ввод: nums1 = [-3,2,2,5,8,19,31], nums2 = [1,2,2,2,6,19,52] Вывод: [2,2,19]
+   * <p>
+   * Пример 2: Ввод: nums1 = [-5,4], nums2 = [1,2] Вывод: []
+   * <p>
+   * Пример 3: Ввод: nums1 = [], nums2 = [1,2] Вывод: []
+   * <p>
+   * Ограничения: len(nums1), len(nums2) >= 0
+   */
+  public static List<Integer> defineCommonNumbers(List<Integer> nums1, List<Integer> nums2) {
+    int p1 = 0;
+    int p2 = 0;
+
+    List<Integer> result = new ArrayList<>();
+
+    while (p1 < nums1.size() && p2 < nums2.size()) {
+      if (nums1.get(p1) < nums2.get(p2)) {
+        p1++;
+      } else if (nums1.get(p1) > nums2.get(p2)) {
+        p2++;
+      } else {
+        result.add(nums1.get(p1));
+        p1++;
+        p2++;
+      }
+    }
+
+    return result;
+  }
+}
